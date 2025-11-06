e71f1574db30568c5272e5ab60a0c43ded45995d
Antonin-Marolleau <antonin.marolleau@edu.esiee.fr>
2025-11-06 08:29:14 +0000

    push 06/11/2025

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+''' Ce code utilise le code de @Antonin-Marolleau dans son intégralité
+# et est soumis aux restrictions du
+# plagiat sur @Antonin-Marolleau dans son intégralité ainsi
+# que du copyright ©2024-2025 pour son
+# auteur @Antonin-Marolleau
+ Il suit le modèle de @Daniel-Courivaud
+'''
+
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
-    # votre code ici
-
-    pass
+    '''
+    return if the parameter @param p is a prime number
+    '''
+    if p<2:
+        return False
+    for i in range(2, int(sqrt(p))+1):
+        if p%i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
+    '''
+    main
+    '''
     # vos appels à la fonction secondaire ici
+
+    print(169, isprime(169))
+    print(5555, isprime(5555))
+    print(3, isprime(3))
 
     for n in range(100):
         if isprime(n):
